d08665488af587f1e8083c1a3ce0001167d2f7ab
Stuart Gendron <stuart.gendron@pwc.com>
2022-05-03 12:52:34 -0700

    first push

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+resource "google_compute_instance" "default" {
+  name         = "test-instance-1"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+}
