96494ef1969a6144c09713236c18eac644613208
Stuart Gendron <stuart.gendron@pwc.com>
2022-05-30 15:45:00 -0700

    added labels + changed zone

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-
+// Compute Instance
 resource "google_compute_instance" "default" {
   name         = "test-instance-1"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  zone         = "us-central1-b"
 
   boot_disk {
     initialize_params {
@@ -14,4 +14,29 @@ resource "google_compute_instance" "default" {
     network = "default"
   }
 
+  labels = {
+    billing     = "102012",
+    dept        = "IT",
+    environment = "sandbox"
+  }
+
+}
+
+// VPC
+resource "google_compute_network" "default" {
+  name = "vpc-network"
+
+}
+
+// Firewall Rule
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["21"]
+  }
+
+  source_tags = ["web"]
 }
