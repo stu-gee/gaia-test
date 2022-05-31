d9b80b94c24b2c24d1f4238fd609c9f50d48ad49
Stuart Gendron <stuart.gendron@pwc.com>
2022-05-31 10:45:28 -0700

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "google_compute_instance" "default" {
   name         = "test-instance-1"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  #zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
